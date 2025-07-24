da86d3540f088d408fb36aae355193a4450b9442
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-07-24 21:03:06 +0200

    forgot to push userservice classs

--- hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/service/HotelService.java
+++ hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/service/HotelService.java
@@ -1,5 +1,32 @@
 package com.medhane.hotelbooking.hotel_booking_system.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.medhane.hotelbooking.hotel_booking_system.model.Hotel;
+import com.medhane.hotelbooking.hotel_booking_system.repository.HotelRepository;
+
+@Service
 public class HotelService {
     
+    @Autowired
+    private HotelRepository hotelRepository;
+    
+    public Hotel getHotel(Long id) {
+        return hotelRepository.findById(id).orElse(null);
+    }
+    
+    public List<Hotel> getAllHotels() {
+        return hotelRepository.findAll();
+    }
+    
+    public Hotel saveHotel(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+    
+    public void deleteHotel(Hotel hotel) {
+        hotelRepository.delete(hotel);
+    }
 }
--- hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/service/UserService.java
+++ hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/service/UserService.java
@@ -1,5 +1,32 @@
 package com.medhane.hotelbooking.hotel_booking_system.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.medhane.hotelbooking.hotel_booking_system.model.User;
+import com.medhane.hotelbooking.hotel_booking_system.repository.UserRepository;
+
+@Service
 public class UserService {
     
+    @Autowired
+    private UserRepository userRepository;
+    
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
 }
