0916756f00b939a6db772aeb6c3e959d7c6edce8
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-07-24 19:54:02 +0200

    working on security and login

---
+++ hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.medhane.hotelbooking.hotel_booking_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin();
+        return http.build();
+    }
+}
