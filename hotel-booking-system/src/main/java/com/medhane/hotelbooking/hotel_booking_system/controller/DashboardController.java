45ea23da4477473ab6736216d86d87e15d79ce71
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-07-27 03:39:50 +0200

    dashboard

---
+++ hotel-booking-system/src/main/java/com/medhane/hotelbooking/hotel_booking_system/controller/DashboardController.java
@@ -0,0 +1,24 @@
+package com.medhane.hotelbooking.hotel_booking_system.controller;
+
+import com.medhane.hotelbooking.hotel_booking_system.model.Room;
+import com.medhane.hotelbooking.hotel_booking_system.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class DashboardController {
+
+    @Autowired
+    private RoomService roomService;
+
+    @GetMapping("/home")
+    public String dashboard(Model model) {
+        List<Room> availableRooms = roomService.getAllRooms(); 
+        model.addAttribute("availableRooms", availableRooms);
+        return "dashboard";
+    }
+}
